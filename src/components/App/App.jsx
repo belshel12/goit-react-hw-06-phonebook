50760482bf37f27e8b22d6c0a74b50d6a4ceb832
Olena Bieletska <belshel12@gmail.com>
2023-04-01 23:31:19 +0300

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Layout } from '../Layout';
+import { Title } from './App.styled';
+import ContactForm from '../ContactForm';
+import ContactList from '../ContactList';
+import Filter from '../Filter';
+
+export const App = () => {
+  return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <Title>Contacts</Title>
+      <Filter />
+      <ContactList />
+    </Layout>
+  );
+};
